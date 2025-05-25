b1e5bced742a53c5ec900771faec3cdbaf66a626
ORB-E <brayamortegarenteria@gmail.com>
2025-05-25 13:57:43 -0600

    modified:   DatosagrupadosNoagrupados.cs #	modified:   Menu.cs #	modified:   Program.cs #	modified:   Utils.cs #	modified:   variablesAleatoriasDiscretas.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using static MiProyecto.Utils;
 using static MiProyecto.Menu;
 
@@ -8,15 +7,4 @@ namespace MiProyecto {
             Menu.MostrarMenu();
         }
     }
-=======
-﻿using static MiProyecto.Utils;
-using static MiProyecto.Menu;
-
-namespace MiProyecto {
-    class Program {
-        static void Main(string[] args) {
-            Menu.MostrarMenu();
-        }
-    }
->>>>>>> 349c920d51cdf481696c247c9555ebe80362ce85
 }
--- variablesAleatoriasDiscretas.cs
+++ variablesAleatoriasDiscretas.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using static MiProyecto.Utils;
 
 namespace MiProyecto
@@ -48,55 +47,4 @@ namespace MiProyecto
             Salir();
         }
     }
-=======
-using static MiProyecto.Utils;
-
-namespace MiProyecto
-{
-    class VariablesAleatoriasDiscretas
-    {
-        public static void EsperanzaMatematica()
-        {
-            var datos = LeerDatos();
-            double resultado = 0;
-            for (int i = 1; i <= datos.Count; i++)
-            {
-                resultado += (datos[i - 1] * i);
-            }
-            resultado /= datos.Count;
-            WriteLine($"La esperanza matemática es: {resultado}");
-            Salir();
-        }
-
-        public static void Varianza()
-        {
-            Clear();
-            WriteLine("\t\tVarianza");
-            var datos = LeerDatos();
-            int n = datos.Count;
-            if (n == 0)
-            {
-                WriteLine("No hay datos para calcular varianza.");
-                return;
-            }
-
-            double media = datos.Average();
-
-            double sumaCuadrados = datos
-                .Select(x => Math.Pow(x - media, 2))
-                .Sum();
-
-            double varianza = sumaCuadrados / (n - 1);
-
-            double desviacion = Math.Sqrt(varianza);
-
-            Clear();
-            WriteLine("\t\tVarianza");
-            WriteLine($"Media: {media:F2}");
-            WriteLine($"Varianza (muestral): {varianza:F2}");
-            WriteLine($"Desviación estándar: {desviacion:F2}");
-            Salir();
-        }
-    }
->>>>>>> 349c920d51cdf481696c247c9555ebe80362ce85
 }
